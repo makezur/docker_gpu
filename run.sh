2e94a6b9d644fb795735e39a967c1e9e4aa0bca3
makezur <makezur@gmail.com>
2018-03-17 22:34:55 +0300

    fixed run

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@ WORKDIR_PATH=$1
 DOCKER_NAME=kochka_gpu
 DOCKER_BINARY=nvidia-docker
 
-$DOCKER_BINARY stop $DOCKER_NAME &> /dev/null  # redirect to /dev/null for silence 
-$DOCKER_BINARY rm $DOCKER_NAME &> /dev/null    #
 $DOCKER_BINARY run -i -t -d --rm \
                -v $WORKDIR_PATH:/playground/ \
                -p 8000:8888 -p 6006:6006 \
