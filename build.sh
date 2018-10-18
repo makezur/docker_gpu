15c90744cf0757047ee6663509790edfb56576ab
Kirill Mazur <kochka@Kirills-MacBook-Pro.local>
2018-10-18 12:13:30 +0300

    update build

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# NAME=kinfu_test
+# DOCKER_BINARY=nvidia-docker
 
-NAME=kochka_gpu
+# nvidia-docker build -t $NAME -f ./Dockerfile .
 
-
-nvidia-docker build -t $NAME -f ./Dockerfile .
+tmux new -s build -d 'sudo nvidia-docker build -t kinfu -f ./Dockerfile  . > build_new.log'
