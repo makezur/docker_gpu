9bd3fbacf25fe96b0f77c9025de6aba1219e53db
Kirill Mazur <makezur@gmail.com>
2018-10-23 20:32:35 +0300

    test

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ DOCKER_BINARY=nvidia-docker
 $DOCKER_BINARY run -i -t -d --rm \
                -p 8000:8888 -p 6006:6006 \
                -e DISPLAY=$DISPLAY \
+               -v /tmp/.X11-unix:/tmp/.X11-unix \
                --volume $XAUTH:/root/.Xauthority \
                --name $DOCKER_NAME \
                $DOCKER_NAME
