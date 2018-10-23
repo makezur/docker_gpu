a8f8e10210d6a2e3db5237aa5d6b786cadea11f3
Kirill Mazur <makezur@gmail.com>
2018-10-23 22:01:39 +0300

    mda

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@ WORKDIR_PATH=$1
 
 DOCKER_NAME=kinfu
 DOCKER_BINARY=nvidia-docker
+DOCKER_RUN_NAME=kinfu_inst
 
-$DOCKER_BINARY run -i -t --network=host -e DISPLAY=$DISPLAY --volume $XAUTH:/root/.Xauthority -d --rm  -p 8000:8888 -p 6006:6006 --name $DOCKER_NAME $DOCKER_NAME
+$DOCKER_BINARY run -i -t --network=host -e DISPLAY=$DISPLAY --volume $XAUTH:/root/.Xauthority -d --rm  -p 8000:8888 -p 6006:6006 --name $DOCKER_RUN_NAME $DOCKER_NAME
 
 # $DOCKER_BINARY exec $DOCKER_NAME bash -c \
 #     "tmux new -d 'jupyter notebook --ip 0.0.0.0 --port 8888 --no-browser --allow-root --NotebookApp.token='"
